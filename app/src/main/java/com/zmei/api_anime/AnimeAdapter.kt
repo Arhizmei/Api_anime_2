b09715bb07e1f14d0da2284e0d24696bb3409d65
Arhizmei <arhizmei@gmail.com>
2023-09-14 10:06:43 +0300

    1.1

--- app/src/main/java/com/zmei/api_anime/AnimeAdapter.kt
+++ app/src/main/java/com/zmei/api_anime/AnimeAdapter.kt
@@ -1,4 +1,37 @@
 package com.zmei.api_anime
 
-class AnimeAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.zmei.api_anime.databinding.ImageItemBinding
+
+class AnimeAdapter : RecyclerView.Adapter<AnimeAdapter.AnimeHolder>() {
+    val animeList = ArrayList<Image_Anime>()
+
+    class AnimeHolder(item: View) : RecyclerView.ViewHolder(item) {
+        private val binding = ImageItemBinding.bind(item)
+        fun bind(imageAnime: Image_Anime) = with(binding) {
+            imageViewAnime.setImageResource(imageAnime.imageId)
+            textTitle.text = imageAnime.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimeHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.image_item, parent, false)
+        return AnimeHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return animeList.size
+    }
+
+    override fun onBindViewHolder(holder: AnimeHolder, position: Int) {
+        holder.bind(animeList[position])
+    }
+
+    fun addImage(imageAnime: Image_Anime) {
+        animeList.add(imageAnime)
+        notifyDataSetChanged()
+    }
 }
--- app/src/main/java/com/zmei/api_anime/MainActivity.kt
+++ app/src/main/java/com/zmei/api_anime/MainActivity.kt
@@ -2,10 +2,25 @@ package com.zmei.api_anime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zmei.api_anime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val adapter = AnimeAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init(){
+        binding.apply {
+            rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            rcView.adapter = adapter
+            buttonAdd
+        }
     }
 }

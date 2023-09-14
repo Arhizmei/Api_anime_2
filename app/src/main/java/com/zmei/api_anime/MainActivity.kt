d4ba42fdaae75647409d98276372d47521caa190
Arhizmei <arhizmei@gmail.com>
2023-09-14 10:14:29 +0300

    1.2

--- app/src/main/java/com/zmei/api_anime/MainActivity.kt
+++ app/src/main/java/com/zmei/api_anime/MainActivity.kt
@@ -9,6 +9,8 @@ import com.zmei.api_anime.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private val adapter = AnimeAdapter()
+    private val imageIdList = listOf(R.drawable.anime1, R.drawable.anime2, R.drawable.anime3)
+    private var index = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,11 +18,16 @@ class MainActivity : AppCompatActivity() {
         init()
     }
 
-    private fun init(){
+    private fun init() {
         binding.apply {
             rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
             rcView.adapter = adapter
-            buttonAdd
+            buttonAdd.setOnClickListener {
+                if (index>2) index = 0
+                val imageAnime = Image_Anime(imageIdList[index], "Image $index")
+                adapter.addImage(imageAnime)
+                index++
+            }
         }
     }
 }

8c5c21efa65ef689ec6a7157973d527d1035fcf2
Arhizmei <arhizmei@gmail.com>
2023-09-19 10:50:05 +0300

    correct load picture

--- app/src/main/java/com/zmei/api_anime/MainActivity.kt
+++ app/src/main/java/com/zmei/api_anime/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zmei.api_anime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -15,24 +16,19 @@ import okhttp3.logging.HttpLoggingInterceptor
 
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: AnimeAdapter
     private lateinit var viewModel: MyViewModel
-    var index = 0
-
+    var loadedItemCount = 0 // Количество загруженных элементов
+    var isLoading = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-
-        val okHttpClient = OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .build()
+        val okHttpClient = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
         val retrofitClient = RetrofitClient(okHttpClient)
-
         binding.rcView.layoutManager = GridLayoutManager(this, 3)
         viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
         adapter = AnimeAdapter(viewModel) { loadMoreData(retrofitClient) }
@@ -42,33 +38,39 @@ class MainActivity : AppCompatActivity() {
                 super.onScrolled(recyclerView, dx, dy)
                 val layoutManager = recyclerView.layoutManager as GridLayoutManager
                 val lastVisibleItem = layoutManager.findLastVisibleItemPosition()
-                val totalItemCount = layoutManager.itemCount
-                if (lastVisibleItem == totalItemCount - 1) {
+                if (!isLoading && lastVisibleItem >= loadedItemCount - 10) {
                     loadMoreData(retrofitClient)
                 }
             }
         })
-
         loadMoreData(retrofitClient)
     }
 
     private fun loadMoreData(retrofitClient: RetrofitClient) {
+        if (isLoading) {
+            return
+        }
+        isLoading = true
         val waifuApiService = retrofitClient.retrofit.create(WaifuApiService::class.java)
         waifuApiService.getWaifuImage().enqueue(object : Callback<ImageModel?> {
             override fun onResponse(call: Call<ImageModel?>, response: Response<ImageModel?>) {
                 if (response.isSuccessful) {
                     val image = response.body()
                     if (image != null) {
-                        val imageAnime = Image_Anime(image, "Image $index")
+                        val imageAnime = Image_Anime(image, "Image ${loadedItemCount + 1}")
                         adapter.addImage(imageAnime)
-                        index++
-
+                        loadedItemCount++
+                        Log.d("mylog", "${loadedItemCount}")
+                        isLoading = false
                     }
                 }
+                isLoading = false
             }
 
             override fun onFailure(call: retrofit2.Call<ImageModel?>, t: Throwable) {
                 Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_LONG).show()
+                isLoading = false
+
             }
         })
     }

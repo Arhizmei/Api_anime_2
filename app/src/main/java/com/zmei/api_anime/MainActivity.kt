3a5a2e1eb33bae53e789279fb6f6e8de9fa91b05
Arhizmei <arhizmei@gmail.com>
2023-11-21 01:35:49 +0200

    refactor view model

--- app/src/main/java/com/zmei/api_anime/MainActivity.kt
+++ app/src/main/java/com/zmei/api_anime/MainActivity.kt
@@ -16,22 +16,17 @@ import okhttp3.logging.HttpLoggingInterceptor
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: AnimeAdapter
+    lateinit var adapter: AnimeAdapter
     private lateinit var viewModel: MyViewModel
-    var loadedItemCount = 0
     var isLoading = false
-    val okHttpClient = OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
-    }).build()
-    val retrofitClient = RetrofitClient(okHttpClient)
-    val waifuApiService = retrofitClient.retrofit.create(WaifuApiService::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.rcView.layoutManager = GridLayoutManager(this, 3)
         viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
-        adapter = AnimeAdapter { loadMoreData() }
+        adapter = AnimeAdapter { viewModel.loadData() }
         binding.rcView.adapter = adapter
 //        binding.rcView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
 //            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
@@ -39,42 +34,18 @@ class MainActivity : AppCompatActivity() {
 //                loadMoreData()
 //            }
 //        })
-        loadMoreData()
+//        loadMoreData()
     }
 
-    private fun loadMoreData() {
-        if (isLoading) {
-            return
-        }
-        isLoading = true
-
-        repeat(20) {
-            waifuApiService.getWaifuImage().enqueue(object : Callback<ImageModel?> {
-                override fun onResponse(
-                    call: Call<ImageModel?>,
-                    response: Response<ImageModel?>
-                ) {
-                    if (response.isSuccessful) {
-                        val image = response.body()
-                        if (image != null) {
-                            val imageAnime = Image_Anime(image, "Image ${loadedItemCount + 1}")
-                            viewModel.imageList.add(imageAnime)
-                            adapter.addImage(viewModel.imageList)
-                            loadedItemCount++
-                            Log.d("mylog", "${loadedItemCount}")
-                            isLoading = false
-                        }
-                    }
-                    isLoading = false
-                }
-
-                override fun onFailure(call: retrofit2.Call<ImageModel?>, t: Throwable) {
-                    Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_LONG).show()
-                    isLoading = false
-
-                }
-            })
-        }
-
-    }
+//    private fun loadMoreData() {
+//        if (isLoading) {
+//            return
+//        }
+//        isLoading = true
+//
+//        repeat(20) {
+//            viewModel.loadData()
+//        }
+//
+//    }
 }

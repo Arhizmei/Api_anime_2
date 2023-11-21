e68007920f793bd596105ca5a3c0aadcc7850cf8
Arhizmei <arhizmei@gmail.com>
2023-11-22 01:57:23 +0200

    use view model for upload image

--- app/src/main/java/com/zmei/api_anime/MainActivity.kt
+++ app/src/main/java/com/zmei/api_anime/MainActivity.kt
@@ -18,34 +18,56 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     lateinit var adapter: AnimeAdapter
     private lateinit var viewModel: MyViewModel
+    var loadedItemCount = 0
     var isLoading = false
-
+    val okHttpClient = OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }).build()
+    val retrofitClient = RetrofitClient(okHttpClient)
+    val waifuApiService = retrofitClient.retrofit.create(WaifuApiService::class.java)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.rcView.layoutManager = GridLayoutManager(this, 3)
         viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
-        adapter = AnimeAdapter { viewModel.loadData() }
+        adapter = AnimeAdapter { loadMoreData() }
         binding.rcView.adapter = adapter
 //        binding.rcView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
 //            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
 //                super.onScrolled(recyclerView, dx, dy)
-//                loadMoreData()
+//                if (!isLoading) {
+//                    isLoading = true
+//                    viewModel.loadMoreData(waifuApiService, loadedItemCount) { success ->
+//                        if (success) {
+//                            loadedItemCount++
+//                            adapter.addImage(viewModel.imageList)
+//                            Log.d("mylog", "${loadedItemCount}")
+//                        } else {
+//                            Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_LONG).show()
+//                        }
+//                        isLoading = false
+//                    }
+//                }
 //            }
 //        })
-//        loadMoreData()
+
+        loadMoreData()
     }
 
-//    private fun loadMoreData() {
-//        if (isLoading) {
-//            return
-//        }
-//        isLoading = true
-//
-//        repeat(20) {
-//            viewModel.loadData()
-//        }
-//
-//    }
+    private fun loadMoreData() {
+        if (!isLoading) {
+            isLoading = true
+            viewModel.loadMoreData(waifuApiService, loadedItemCount) { success ->
+                if (success) {
+                    loadedItemCount++
+                    adapter.addImage(viewModel.imageList)
+                    Log.d("mylog", "${loadedItemCount}")
+                } else {
+                    Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_LONG).show()
+                }
+                isLoading = false
+            }
+        }
+    }
 }
--- app/src/main/java/com/zmei/api_anime/MyViewModel.kt
+++ app/src/main/java/com/zmei/api_anime/MyViewModel.kt
@@ -11,40 +11,28 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MyViewModel(private val act: MainActivity) : ViewModel() {
-    val imageList = mutableListOf<Image_Anime>()
-    private val okHttpClient =
-        OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }).build()
-    private val retrofitClient = RetrofitClient(okHttpClient)
-    private val waifuApiService = retrofitClient.retrofit.create(WaifuApiService::class.java)
-    var loadedItemCount = 0
+class MyViewModel : ViewModel() {
+    val imageList: MutableList<Image_Anime> = mutableListOf<Image_Anime>()
 
-    fun loadData() {
-        waifuApiService.getWaifuImage().enqueue(object : Callback<ImageModel?> {
-            override fun onResponse(
-                call: Call<ImageModel?>,
-                response: Response<ImageModel?>
-            ) {
+    fun loadMoreData(apiService: WaifuApiService, loadedItemCount: Int, callback: (Boolean) -> Unit) {
+        apiService.getWaifuImage().enqueue(object : Callback<ImageModel?> {
+            override fun onResponse(call: Call<ImageModel?>, response: Response<ImageModel?>) {
                 if (response.isSuccessful) {
                     val image = response.body()
                     if (image != null) {
                         val imageAnime = Image_Anime(image, "Image ${loadedItemCount + 1}")
                         imageList.add(imageAnime)
-                        act.adapter.addImage(imageList)
-                        loadedItemCount++
-                        Log.d("mylog", "$loadedItemCount")
-                        act.isLoading = false
+                        callback(true)
+                    } else {
+                        callback(false)
                     }
+                } else {
+                    callback(false)
                 }
-                act.isLoading = false
             }
 
             override fun onFailure(call: retrofit2.Call<ImageModel?>, t: Throwable) {
-                Toast.makeText(act, "Error", Toast.LENGTH_LONG).show()
-                act.isLoading = false
-
+                callback(false)
             }
         })
     }
